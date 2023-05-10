9f72c2d40d81fa2fcf72e33287076c36f6165000
Jonne Kokkonen <jonne.kokkonen@gmail.com>
2023-05-10 22:15:46 +0300

    add basic sdl stuff

--- main.cpp
+++ main.cpp
@@ -2,8 +2,11 @@
 #include "SDL_keycode.h"
 #include "SDL_log.h"
 #include "SDL_video.h"
-#include <SDL.h>
 #include "gambatte.h"
+
+#include <SDL.h>
+#include <stdlib.h>
+#include <vector>
 
 using namespace gambatte;
 
@@ -11,30 +14,49 @@ SDL_Window *win;
 SDL_Renderer *rend;
 SDL_Texture *maintexture;
 
+gambatte::uint_least32_t *videoBuf;
+std::ptrdiff_t pitch;
+gambatte::uint_least32_t *audioBuf;
+std::size_t samples;
+
+const int texture_width = 160;
+const int texture_height = 144;
+
+uint32_t *framebuffer;
+
 gambatte::GB gb_;
 
-struct GbVidBuf;
+static SDL_AudioDeviceID devid_out = 0;
 
-GbVidBuf setPixelBuffer(void *pixels, PixelBuffer::PixelFormat format, std::ptrdiff_t pitch);
+int audio_init(int audio_buffer_size, const char *output_device_name) {
 
-struct ButtonInfo {
-  char const *label;
-  char const *category;
-  int defaultKey;
-  int defaultAltKey;
-  unsigned char defaultFpp;
-};
+  SDL_AudioSpec want_out, have_out;
 
-static ButtonInfo const buttonInfoGbUp = { "Up", "Game", SDLK_UP, 0, 0 };
-static ButtonInfo const buttonInfoGbDown = { "Down", "Game", SDLK_DOWN, 0, 0 };
-static ButtonInfo const buttonInfoGbLeft = { "Left", "Game", SDLK_LEFT, 0, 0 };
-static ButtonInfo const buttonInfoGbRight = { "Right", "Game", SDLK_RIGHT, 0, 0 };
-static ButtonInfo const buttonInfoGbA = { "A", "Game", SDLK_x, 0, 0 };
-static ButtonInfo const buttonInfoGbB = { "B", "Game", SDLK_z, 0, 0 };
-static ButtonInfo const buttonInfoGbStart = { "Start", "Game", SDLK_s, 0, 0 };
-static ButtonInfo const buttonInfoGbSelect = { "Select", "Game", SDLK_a, 0, 0 };
+  // Open output device first to avoid possible Directsound errors
+  SDL_zero(want_out);
+  want_out.freq = 44100;
+  want_out.format = AUDIO_S16;
+  want_out.channels = 2;
+  want_out.samples = audio_buffer_size;
+  devid_out = SDL_OpenAudioDevice(output_device_name, 0, &want_out, &have_out,
+                                  SDL_AUDIO_ALLOW_ANY_CHANGE);
+  if (devid_out == 0) {
+    SDL_Log("Failed to open output: %s", SDL_GetError());
+    return 0;
+  }
 
+  // Start audio processing
+  SDL_Log("Opening audio devices");
+  SDL_PauseAudioDevice(devid_out, 0);
 
+  return 1;
+}
+
+void audio_destroy() {
+  SDL_Log("Closing audio devices");
+  SDL_PauseAudioDevice(devid_out, 1);
+  SDL_CloseAudioDevice(devid_out);
+}
 
 int initialize_sdl() {
   const int window_width = 640;  // SDL window width
@@ -42,7 +64,7 @@ int initialize_sdl() {
 
   if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
     SDL_LogCritical(SDL_LOG_CATEGORY_ERROR, "SDL_Init: %s\n", SDL_GetError());
-    return 0;
+    return -1;
   }
 
   // SDL documentation recommends this
@@ -55,40 +77,81 @@ int initialize_sdl() {
 
   rend = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
 
-  SDL_RenderSetLogicalSize(rend, 320, 240);
+  SDL_RenderSetLogicalSize(rend, texture_width, texture_height);
 
+  // Initialize an SDL texture and a raw framebuffer
   maintexture = SDL_CreateTexture(rend, SDL_PIXELFORMAT_ARGB8888,
-                                  SDL_TEXTUREACCESS_TARGET, 320, 240);
+                                  SDL_TEXTUREACCESS_TARGET, texture_width,
+                                  texture_height);
+
+  framebuffer = new gambatte::uint_least32_t[texture_width * texture_height];
+  SDL_memset4(framebuffer, 0xA0000000, texture_width * texture_height);
 
   // clear the main texture
   SDL_SetRenderTarget(rend, maintexture);
   SDL_SetRenderDrawColor(rend, 0, 0, 0, 1);
   SDL_RenderClear(rend);
 
+  // Initialize audio using system default output
+  audio_init(512, NULL);
+
   SDL_LogSetAllPriority(SDL_LOG_PRIORITY_DEBUG);
-  return 1;
+  return 0;
 }
 
-int destroy_sdl(){
+int destroy_sdl() {
   SDL_DestroyTexture(maintexture);
   SDL_DestroyRenderer(rend);
   SDL_DestroyWindow(win);
   SDL_Quit();
-  return 1;
+  return 0;
+}
+
+int render_sdl() {
+  SDL_SetRenderTarget(rend, NULL);
+  SDL_SetRenderDrawColor(rend, 0, 0, 0, 0);
+  SDL_RenderClear(rend);
+  SDL_RenderCopy(rend, maintexture, NULL, NULL);
+  SDL_RenderPresent(rend);
+  SDL_SetRenderTarget(rend, maintexture);
+  return 0;
 }
 
 int main(int argc, char *argv[]) {
 
-  int result = 0;
-  result = initialize_sdl();
+  int err = 0;
+  err = initialize_sdl();
 
-  if (result == 0) {
+  if (err != 0) {
     SDL_LogError(SDL_LOG_CATEGORY_SYSTEM, "Could not initialize SDL.");
     destroy_sdl();
     return 1;
   }
 
+  err = gb_.loadBios("gbc_bios.bin", 0, 0);
+  if (err != 0) {
+    SDL_LogError(SDL_LOG_CATEGORY_SYSTEM, "Could not load BIOS");
+    destroy_sdl();
+    return 1;
+  }
 
+  err = gb_.load("lsdj.gb", 0);
+  if (err != 0) {
+    SDL_LogError(SDL_LOG_CATEGORY_SYSTEM, "Could not load ROM");
+    destroy_sdl();
+    return 1;
+  }
+
+  for (;;) {
+    gb_.runFor(framebuffer, 160, gambatte::uint_least32_t *audioBuf, std::size_t &samples)
+    render_sdl();
+    SDL_Delay(100);
+  }
+
+  //  SDL_UpdateTexture(maintexture, NULL, framebuffer, texture_width *
+  //  sizeof(framebuffer[0]));
+
+  destroy_sdl();
 
   return 0;
 }
