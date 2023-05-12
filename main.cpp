65f71ff03bd46500a17e3595b49f84b208138c8d
Jonne Kokkonen <jonne.kokkonen@gmail.com>
2023-05-12 07:37:39 +0300

    make image look somewhat right

--- main.cpp
+++ main.cpp
@@ -1,26 +1,37 @@
+#include "SDL_audio.h"
 #include "SDL_events.h"
 #include "SDL_keycode.h"
 #include "SDL_log.h"
+#include "SDL_pixels.h"
+#include "SDL_render.h"
+#include "SDL_stdinc.h"
 #include "SDL_video.h"
 #include "gambatte.h"
+#include "gbint.h"
 
 #include <SDL.h>
+#include <cstddef>
 #include <stdlib.h>
 #include <vector>
+#include <unistd.h>
 
 using namespace gambatte;
 
 SDL_Window *win;
 SDL_Renderer *rend;
 SDL_Texture *maintexture;
-
-gambatte::uint_least32_t *videoBuf;
-std::ptrdiff_t pitch;
-gambatte::uint_least32_t *audioBuf;
-std::size_t samples;
+SDL_Texture *target_texture;
 
 const int texture_width = 160;
 const int texture_height = 144;
+
+gambatte::uint_least32_t *videoBuf =
+    new uint32_t[texture_width * texture_height];
+uint32_t *audiobuffer;
+std::size_t samples = 512;
+std::ptrdiff_t pitch = 160;
+
+SDL_AudioSpec want_out, have_out;
 
 uint32_t *framebuffer;
 
@@ -28,9 +39,29 @@ gambatte::GB gb_;
 
 static SDL_AudioDeviceID devid_out = 0;
 
+int render_sdl() {
+  SDL_SetRenderTarget(rend, NULL);
+  SDL_SetRenderDrawColor(rend, 0, 0, 0, 0);
+  SDL_RenderClear(rend);
+  SDL_RenderCopy(rend, maintexture, NULL, NULL);
+  SDL_RenderPresent(rend);
+  return 0;
+}
+
+void audio_callback(void *userdata, Uint8 *stream, int len) {
+  uint *bytes = nullptr;
+  int maintexture_pitch = 0;
+  std::ptrdiff_t smps;
+  SDL_zero(stream);
+
+  smps = gb_.runFor(videoBuf, pitch, audiobuffer, samples);
+  if (smps != -1) {
+  }
+}
+
 int audio_init(int audio_buffer_size, const char *output_device_name) {
 
-  SDL_AudioSpec want_out, have_out;
+  SDL_Log("Initialize audio");
 
   // Open output device first to avoid possible Directsound errors
   SDL_zero(want_out);
@@ -40,13 +71,14 @@ int audio_init(int audio_buffer_size, const char *output_device_name) {
   want_out.samples = audio_buffer_size;
   devid_out = SDL_OpenAudioDevice(output_device_name, 0, &want_out, &have_out,
                                   SDL_AUDIO_ALLOW_ANY_CHANGE);
+  SDL_Log("Opened device %s", SDL_GetAudioDeviceName(0, 0));
   if (devid_out == 0) {
     SDL_Log("Failed to open output: %s", SDL_GetError());
     return 0;
   }
 
   // Start audio processing
-  SDL_Log("Opening audio devices");
+
   SDL_PauseAudioDevice(devid_out, 0);
 
   return 1;
@@ -70,22 +102,21 @@ int initialize_sdl() {
   // SDL documentation recommends this
   atexit(SDL_Quit);
 
-  win = SDL_CreateWindow("m8c", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                         window_width, window_height,
+  win = SDL_CreateWindow("worlds no1 bestest emulator", SDL_WINDOWPOS_CENTERED,
+                         SDL_WINDOWPOS_CENTERED, window_width, window_height,
                          SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL |
-                             SDL_WINDOW_RESIZABLE | SDL_WINDOW_FULLSCREEN);
+                             SDL_WINDOW_RESIZABLE);
 
   rend = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
 
   SDL_RenderSetLogicalSize(rend, texture_width, texture_height);
 
   // Initialize an SDL texture and a raw framebuffer
-  maintexture = SDL_CreateTexture(rend, SDL_PIXELFORMAT_ARGB8888,
-                                  SDL_TEXTUREACCESS_TARGET, texture_width,
+  maintexture = SDL_CreateTexture(rend, SDL_PIXELFORMAT_RGB888,
+                                  SDL_TEXTUREACCESS_STREAMING, texture_width,
                                   texture_height);
 
-  framebuffer = new gambatte::uint_least32_t[texture_width * texture_height];
-  SDL_memset4(framebuffer, 0xA0000000, texture_width * texture_height);
+  audiobuffer = new gambatte::uint_least32_t[1024 * 10];
 
   // clear the main texture
   SDL_SetRenderTarget(rend, maintexture);
@@ -100,6 +131,7 @@ int initialize_sdl() {
 }
 
 int destroy_sdl() {
+  audio_destroy();
   SDL_DestroyTexture(maintexture);
   SDL_DestroyRenderer(rend);
   SDL_DestroyWindow(win);
@@ -107,19 +139,31 @@ int destroy_sdl() {
   return 0;
 }
 
-int render_sdl() {
-  SDL_SetRenderTarget(rend, NULL);
-  SDL_SetRenderDrawColor(rend, 0, 0, 0, 0);
-  SDL_RenderClear(rend);
-  SDL_RenderCopy(rend, maintexture, NULL, NULL);
-  SDL_RenderPresent(rend);
-  SDL_SetRenderTarget(rend, maintexture);
-  return 0;
+std::ptrdiff_t run(uint_least32_t *pixels, std::ptrdiff_t pitch,
+                   uint_least32_t *soundBuf, std::size_t &samples) {
+  std::size_t targetSamples = samples;
+  std::size_t actualSamples = 0;
+  std::ptrdiff_t vidFrameSampleNo = -1;
+
+  while (actualSamples < targetSamples && vidFrameSampleNo < 0) {
+    samples = targetSamples - actualSamples;
+    std::ptrdiff_t const vfsn =
+        gb_.runFor(pixels, pitch, soundBuf + actualSamples, samples);
+
+    if (vfsn >= 0)
+      vidFrameSampleNo = actualSamples + vfsn;
+
+    actualSamples += samples;
+  }
+  samples = actualSamples;
+  return vidFrameSampleNo;
 }
 
 int main(int argc, char *argv[]) {
 
+  SDL_Event e;
   int err = 0;
+
   err = initialize_sdl();
 
   if (err != 0) {
@@ -135,7 +179,7 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  err = gb_.load("lsdj.gb", 0);
+  err = gb_.load("lsdj.gb", GB::CGB_MODE);
   if (err != 0) {
     SDL_LogError(SDL_LOG_CATEGORY_SYSTEM, "Could not load ROM");
     destroy_sdl();
@@ -143,13 +187,35 @@ int main(int argc, char *argv[]) {
   }
 
   for (;;) {
-    gb_.runFor(framebuffer, 160, gambatte::uint_least32_t *audioBuf, std::size_t &samples)
-    render_sdl();
-    SDL_Delay(100);
+
+    uint *bytes = nullptr;
+    int maintexture_pitch = 0;
+    std::ptrdiff_t smps;
+
+    while (SDL_PollEvent(&e) != 0) {
+      switch (e.type) {
+      case SDL_QUIT:
+        destroy_sdl();
+        return 0;
+      }
+    }
+
+    smps = run(videoBuf, pitch, audiobuffer, samples);
+    SDL_QueueAudio(devid_out, audiobuffer, samples);
+    if (smps >= 0) {
+      
+      SDL_LockTexture(maintexture, nullptr, reinterpret_cast<void **>(&bytes),
+                      &maintexture_pitch);
+      SDL_memcpy(bytes, videoBuf,
+                 texture_width * texture_height * sizeof(uint32_t));
+      SDL_UnlockTexture(maintexture);
+      render_sdl();
+      const int ft = (16743ul - 16743 / 1024);
+      usleep(ft);
+    }
   }
 
-  //  SDL_UpdateTexture(maintexture, NULL, framebuffer, texture_width *
-  //  sizeof(framebuffer[0]));
+  // run(bytes, pitch, audiobuffer, samples);
 
   destroy_sdl();
 
